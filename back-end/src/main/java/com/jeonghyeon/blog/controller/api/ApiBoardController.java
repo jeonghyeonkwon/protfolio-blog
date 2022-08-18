f9b28fdd2ce2b9ae2dc5e6783dc8c2cea96c6073
jeonghyeonkwon <givejeong2468@gmail.com>
2022-08-18 11:33:47 +0900

    게스글 작성 api 작업 완료

--- back-end/src/main/java/com/jeonghyeon/blog/controller/api/ApiBoardController.java
+++ back-end/src/main/java/com/jeonghyeon/blog/controller/api/ApiBoardController.java
@@ -1,18 +1,44 @@
 package com.jeonghyeon.blog.controller.api;
 
+import com.jeonghyeon.blog.dto.BoardRequest;
+import com.jeonghyeon.blog.exhandler.CustomValidationException;
+import com.jeonghyeon.blog.exhandler.ValidationExceptionDto;
+import com.jeonghyeon.blog.security.util.SecurityUtil;
+import com.jeonghyeon.blog.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/")
 @Slf4j
 public class ApiBoardController {
+    private final BoardService boardService;
+
     @GetMapping("/")
-    public ResponseEntity board(){
+    public ResponseEntity boardList(){
         return new ResponseEntity("테스트",HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity writeBoard(@RequestBody @Valid BoardRequest dto, BindingResult result){
+        Optional<String> opUserId = SecurityUtil.getCurrnetAccountId();
+        if(!opUserId.isPresent()) throw new IllegalStateException("잘못된 요청입니다. 로그인 후 이용해 주세요");
+        if(result.hasErrors()){
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            ValidationExceptionDto errorList = new ValidationExceptionDto(fieldErrors);
+            throw new CustomValidationException("검증 오류",errorList);
+        }
+        boardService.writeBoard(opUserId.get(),dto);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
--- back-end/src/main/java/com/jeonghyeon/blog/service/BoardService.java
+++ back-end/src/main/java/com/jeonghyeon/blog/service/BoardService.java
@@ -1,4 +1,34 @@
 package com.jeonghyeon.blog.service;
 
+import com.jeonghyeon.blog.dto.BoardRequest;
+import com.jeonghyeon.blog.entity.Account;
+import com.jeonghyeon.blog.entity.Board;
+import com.jeonghyeon.blog.repository.AccountRepository;
+import com.jeonghyeon.blog.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BoardService {
+    private final AccountRepository accountRepository;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public String writeBoard(String userId, BoardRequest dto) {
+        Optional<Account> opAccount = accountRepository.findByUserId(userId);
+        if(!opAccount.isPresent()) throw new IllegalStateException("로그인 한 회원과 일치하는 유저 정보가 없습니다. 다시 시도해 주세요");
+        Account account = opAccount.get();
+        Board board = new Board(UUID.randomUUID().toString(),dto.getTitle(),dto.getContent(),account);
+        Board savedBoard = boardRepository.save(board);
+        return savedBoard.getUuid();
+    }
+
+
+    
 }
